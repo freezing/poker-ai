032ca95fee63955a5bf5e8726a6af1a8f5df87d4
freezing <nikolavla@gmail.com>
2016-12-26 12:07:06 +0000

    Minor: remove :

--- src/main/java/io/freezing/ai/domain/HandCategory.java
+++ src/main/java/io/freezing/ai/domain/HandCategory.java
@@ -2,5 +2,5 @@ package io.freezing.ai.domain;
 
 public enum HandCategory {
     NO_PAIR, PAIR, TWO_PAIR, THREE_OF_A_KIND, STRAIGHT,
-    FLUSH, FULL_HOUSE, FOUR_OF_A_KIND, STRAIGHT_FLUSH;
+    FLUSH, FULL_HOUSE, FOUR_OF_A_KIND, STRAIGHT_FLUSH
 }
