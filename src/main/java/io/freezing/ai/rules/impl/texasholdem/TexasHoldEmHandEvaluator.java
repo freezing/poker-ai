cb3e213c3fafac80f100c91e9888445601ed0d79
freezing <nikolavla@gmail.com>
2016-12-27 12:21:07 +0000

    Hand evaluator doesn't need 7 cards

--- src/main/java/io/freezing/ai/rules/impl/texasholdem/TexasHoldEmHandEvaluator.java
+++ src/main/java/io/freezing/ai/rules/impl/texasholdem/TexasHoldEmHandEvaluator.java
@@ -1,7 +1,6 @@
 package io.freezing.ai.rules.impl.texasholdem;
 
 import io.freezing.ai.domain.*;
-import io.freezing.ai.function.BitUtils;
 import io.freezing.ai.function.CardUtils;
 import io.freezing.ai.rules.HandEvaluator;
 
@@ -10,13 +9,6 @@ public class TexasHoldEmHandEvaluator implements HandEvaluator {
     public EvaluatedHand evaluate(Hand hand, Table table) {
         Card[] cards = CardUtils.merge(table, hand);
         long handBitmask = createBitmask(cards);
-
-        // Make sure that we have 7 different cards in the game
-        int setBits = 0;
-        for (int i = 0; i < 4; i++) setBits += BitUtils.countSetBits( (int)((handBitmask >> (16 * i)) & 0xFFFF));
-        if (setBits != 7) {
-            throw new IllegalArgumentException(String.format("Expected 7 different cards, but got: %s", CardUtils.buildString(handBitmask)));
-        }
 
         int rank = TexasHoldEmEval.evaluate(handBitmask);
         return new EvaluatedHand(new WholeHand(hand, table), rank);
