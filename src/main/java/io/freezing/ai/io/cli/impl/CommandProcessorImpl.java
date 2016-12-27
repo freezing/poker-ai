ab90ae373d5116baa71f0787d3f1c43268528347
Nikola Stojiljkovic <nikolavla@gmail.com>
2016-12-27 22:55:59 +0000

    Refactor

--- src/main/java/io/freezing/ai/io/cli/impl/CommandProcessorImpl.java
+++ src/main/java/io/freezing/ai/io/cli/impl/CommandProcessorImpl.java
@@ -20,18 +20,21 @@ public class CommandProcessorImpl implements CommandProcessor {
 
     @Override
     public void process(Command command) {
-        logger.info(String.format("About to process command: %s", command.toString()));
-        if (command instanceof EvaluatePokerStateCommand) {
-            EvaluatePokerStateCommand cmd = (EvaluatePokerStateCommand) command;
-            try {
-                this.pokerOutputStream.write(String.format("%s\n", cmd.getLine()).getBytes(Charset.defaultCharset()));
-            } catch (IOException e) {
-                // TODO: Handle command processor exceptions properly (use exception handler)
-                e.printStackTrace();
+        try {
+            logger.info(String.format("About to process command: %s", command.toString()));
+            if (command instanceof EvaluatePokerStateCommand) {
+                evaluatePokerStateCommand((EvaluatePokerStateCommand) command);
+            } else {
+                logger.warning(String.format("Unhandled command: %s", command.getClass().getName()));
             }
-        } else {
-            logger.warning(String.format("Unhandled command: %s", command.getClass().getName()));
+        } catch (Exception e) {
+            logger.severe(e.getLocalizedMessage());
+            e.printStackTrace();
         }
+    }
+
+    private void evaluatePokerStateCommand(EvaluatePokerStateCommand cmd) throws IOException {
+        this.pokerOutputStream.write(String.format("%s\n", cmd.getLine()).getBytes(Charset.defaultCharset()));
     }
 
     @Override
