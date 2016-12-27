967701140a0c68d02d97ebf76629fcff6e88d4a3
freezing <nikolavla@gmail.com>
2016-12-27 09:25:17 +0000

    Add parallel stream performance test

--- src/test/java/io/freezing/ai/performance/TexasHoldEmEvaluatorSpeedTest.java
+++ src/test/java/io/freezing/ai/performance/TexasHoldEmEvaluatorSpeedTest.java
@@ -11,18 +11,21 @@ import io.freezing.ai.rules.impl.texasholdem.TexasHoldEmHandEvaluator;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TexasHoldEmEvaluatorSpeedTest {
-    private HandEvaluator evaluator = new TexasHoldEmHandEvaluator();
-    private Random rnd = new Random(1453243L);
+    static private final HandEvaluator evaluator = new TexasHoldEmHandEvaluator();
+    static private final Random rnd = new Random(1453243L);
 
-    @Test
-    public void speedTest() {
-        Hand hands[] = new Hand[1000];
-        Table tables[] = new Table[hands.length];
+    static private final Card[] cards = new Card[52];
+    static private final Hand hands[] = new Hand[1000];
+    static private final Table tables[] = new Table[hands.length];
+    static private final int EVALUATIONS = 1000000;
 
-        Card[] cards = new Card[52];
+    static {
         for (int i = 0; i < cards.length; i++) cards[i] = CardUtils.getCard(i);
 
         for (int i = 0; i < hands.length; i++) {
@@ -30,15 +33,36 @@ public class TexasHoldEmEvaluatorSpeedTest {
             hands[i] = new Hand(new Card[]{ cards[0], cards[1] });
             tables[i] = new Table(new Card[]{ cards[2], cards[3], cards[4], cards[5], cards[6] });
         }
+    }
 
-        final int EVALUATIONS = 1000000;
-
+    @Test
+    public void speedTestSequential() {
+        System.out.println("Performance testing using sequential iteration...");
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < EVALUATIONS; i++) {
             int idx = rnd.nextInt(hands.length);
             EvaluatedHand evalHand = evaluator.evaluate(hands[idx], tables[idx]);
             Assert.assertTrue(evalHand.getRank() > 0);
         }
+        long endTime = System.currentTimeMillis();
+
+        long totalTimeMs = endTime - startTime;
+        double totalSeconds = totalTimeMs / 1000.0;
+        double evaluationsPerSecond = EVALUATIONS / totalSeconds;
+
+        System.out.println(String.format("Total time to evaluate %d hands: %f", EVALUATIONS, totalSeconds));
+        System.out.println(String.format("Evaluations per second: %f", evaluationsPerSecond));
+    }
+
+    @Test
+    public void speedTestParallelStream() {
+        System.out.println("Performance testing using parallel stream...");
+        long startTime = System.currentTimeMillis();
+        List<EvaluatedHand> evalHands = Stream.iterate(0, a -> a + 1).limit(EVALUATIONS).parallel().map(i -> {
+            int idx = rnd.nextInt(hands.length);
+            return evaluator.evaluate(hands[idx], tables[idx]);
+        }).collect(Collectors.toList());
+        Assert.assertTrue(evalHands.size() == EVALUATIONS);
         long endTime = System.currentTimeMillis();
 
         long totalTimeMs = endTime - startTime;
