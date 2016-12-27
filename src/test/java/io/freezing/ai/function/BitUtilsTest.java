f2ef4cc7963a1b793adb73ba6b33c17511442659
freezing <nikolavla@gmail.com>
2016-12-27 12:30:24 +0000

    BitUtils tests

--- src/test/java/io/freezing/ai/function/BitUtilsTest.java
+++ src/test/java/io/freezing/ai/function/BitUtilsTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class BitUtilsTest {
     @Test
-    public void testCountSetBits() {
+    public void testCountSetBitsInt() {
         Assert.assertTrue(4 == BitUtils.countSetBits(15));
         Assert.assertTrue(1 == BitUtils.countSetBits(8));
         Assert.assertTrue(25 == BitUtils.countSetBits(2147483150));
@@ -17,7 +17,30 @@ public class BitUtilsTest {
         }
 
         for (int k = 0; k < 24; k++) {
-            Assert.assertTrue(4 == BitUtils.countSetBits(0x000F << k));
+            Assert.assertTrue(4 == BitUtils.countSetBits(0x0F << k));
+            Assert.assertTrue(4 == BitUtils.countSetBits(0xF0 << k));
+            Assert.assertTrue(8 == BitUtils.countSetBits(0xFF << k));
+        }
+    }
+
+    @Test
+    public void testCountSetBitsLong() {
+        Assert.assertTrue(4 == BitUtils.countSetBits(15L));
+        Assert.assertTrue(1 == BitUtils.countSetBits(8L));
+        Assert.assertTrue(25 == BitUtils.countSetBits(2147483150L));
+        Assert.assertTrue(12 == BitUtils.countSetBits(17920377L));
+
+        // Test all versions of one bit
+        for (int k = 0; k < 64; k++) {
+            Assert.assertTrue(1 == BitUtils.countSetBits(1L << k));
+        }
+
+        for (int k = 0; k < 64 - 4*4; k++) {
+            Assert.assertTrue(4  == BitUtils.countSetBits(0x000FL << k));
+            Assert.assertTrue(8  == BitUtils.countSetBits(0xF00FL << k));
+            Assert.assertTrue(12 == BitUtils.countSetBits(0xF0FFL << k));
+            Assert.assertTrue(12 == BitUtils.countSetBits(0xFF0FL << k));
+            Assert.assertTrue(16 == BitUtils.countSetBits(0xFFFFL << k));
         }
     }
 }
